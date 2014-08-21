9132e386468428cf4cc2a16cd9f42b2350113a59
Freeman Joe <freimanjoe@gmail.com>
2014-08-21 20:28:34 +0800

    functions finished
    Completed makecachematrix and cachesolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## JHU R Programming Assignment 2
+## makeCacheMatrix will return a matrix object which is invertible.
 
-## Write a short comment describing this function
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse_of_x <- NULL
+    set <- function(y) {
+        x <<- y
+        inverse_of_x <<- NULL
+    }
+    get <- function() x
+    set_inverse <- function(solve) inverse_of_x <<- solve
+    get_inverse <- function() inverse_of_x
+    list(set = set, get = get,
+         set_inverse = set_inverse,
+         get_inverse = get_inverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve will try to solve the matrix if no cached inversed matrix is found.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse_of_x <- x$get_inverse()
+    if(!is.null(inverse_of_x)) {
+        message("getting cached data")
+        return(inverse_of_x)
+    }
+    data <- x$get()
+    inverse_of_x <- solve(data, ...)
+    x$set_inverse(inverse_of_x)
+    inverse_of_x
 }
